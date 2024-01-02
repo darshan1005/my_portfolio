e9d036b404965c3ea4bb864a4bbd72b7110e7970
darshan1005 <battuladarshan7838@gmail.com>
2024-01-02 18:20:44 +0530

    my-app update

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -18,53 +18,69 @@ import dogs from '../../assets/dogs.png'
 import cepheus from '../../assets/cephesu.png'
 import hira from '../../assets/hira.png'
 import found from '../../assets/foundit.png'
+import vcp from '../../assets/vc-player.png'
 
 
 export const projectsData = [
     {
         id: 1,
+        image: sher,
+        title: "Sherlock_Holems_Game",
+        category: "game",
+        link: "https://sherlockholmes-puzzels.netlify.app",
+    }, 
+    {
+        id: 2,
+        image: icdect,
+        title: "ICDECT_ANITS_2024",
+        category: "Web",
+        link: "https://github.com/darshan1005/ICDECT_ANITS_2024",
+    }, 
+    {
+        id: 3,
+        image: vcp,
+        title: "custom-video-player",
+        category: "web",
+        link: "https://darshan1005.github.io/video_player/"
+    }, 
+    {
+        id: 4,
         image: pswdgen,
         title: "RandomPSWD_Generator",
         category: "web",
         link: "https://darshan1005.github.io/randomPSWD_generator/",
     },
     {
-        id: 2,
+        id: 5,
         image: grk,
         title: "GRK_Insta",
         category: "web",
         link: "https://grkfashion.netlify.app",
     },
     {
-        id: 3,
+        id: 6,
         image: qr,
         title: "QR_card",
         category: "web",
         link: "https://darshan1005.github.io/QR_code/",
     },
+    
     {
-        id: 4,
-        image: sher,
-        title: "Sherlock_Holems_Game",
-        category: "game",
-        link: "https://sherlockholmes-puzzels.netlify.app",
-    },
-    {
-        id: 5,
+        id: 7,
         image: country,
         title: "Country_Details_Card",
         category: "web",
         link: "https://darshan1005.github.io/Country_Details/",
     },
     {
-        id: 6,
+        id: 8,
         image: synonyms,
         title: "Synonyms_Search_card",
         category: "web",
         link: "https://darshan1005.github.io/synonyms_Search/"
     },
     {
-        id: 7,
+        id: 9,
         image: bmi,
         title: "BMI_Calculator",
         category: "web",
@@ -72,96 +88,90 @@ export const projectsData = [
 
     },
     {
-        id: 8,
+        id: 10,
         image: cocktail,
         title: "Cocktail_Card",
         category: "web",
         link: "https://darshan1005.github.io/cocktail_app/"
     }, 
     {
-        id: 9,
-        image: icdect,
-        title: "ICDECT_ANITS_2024",
-        category: "Web",
-        link: "https://github.com/darshan1005/ICDECT_ANITS_2024",
-    },
-    {
-        id: 10,
+        id: 11,
         image: quiz,
         title: "QUIZ_Web",
         category: "Web",
         link: "https://darshan1005.github.io/quiz_app/"
     }, 
     {
-        id: 11,
+        id: 12,
         image: fly,
         title: "Fly_App",
         category: "Web",
         link: "https://dreamflight.netlify.app"
     },
     {
-        id: 12,
+        id: 13,
         image: ecommers,
         title: "E-Commers",
         category: "Web",
         link: "https://github.com/darshan1005/E-commers"
     },
     {
-        id: 13,
+        id: 14,
         image: scholars,
         title: "Scholarsviu.com",
         category: "web",
         link: "https://github.com/darshan1005/scholarsviu.com",
     },
     {
-        id: 14,
+        id: 15,
         image: captcha,
         title: "Captcha_Generator",
         category: "web",
         link: "https://darshan1005.github.io/Captcha_generator/"
     },
     {
-        id: 15,
+        id: 16,
         image: memory,
         title: "Memory_Game",
         category: "game",
         link: "https://darshan1005.github.io/memory_card_game/"
     },
     {
-        id: 16,
+        id: 17,
         image: bus,
         title: "BUS-CARD_SYSTEM",
         category: "design",
         link: "https://www.figma.com/file/vWzAaHaEmmR69zFo8sGYLs/BUS-CARD-SYSTEM?type=design&node-id=0%3A1&mode=design&t=zq1IUTm5Qmk3JX0L-1"
     },
     {
-        id: 17,
+        id: 18,
         image: dogs,
         title: "Stray Dog",
         category: "design",
         link: "https://www.figma.com/file/49j6fbCDVfRmFtjdpDKju1/street-dog-web-site?type=design&node-id=0%3A1&mode=design&t=wtbinboIUqymFA77-1"
     },
     {
-        id: 18,
+        id: 19,
         image: found,
         title: "foundit",
         category: "design",
         link: "https://www.figma.com/file/oYGDfwjLNuy9QMQD1F2q9n/foundit?type=design&node-id=0%3A1&mode=design&t=QdmLYCvEt5AWTAg2-1"
     },
     {
-        id: 19,
+        id: 20,
         image: hira,
         title: "Hira yoga",
         category: "design",
         link: "https://www.figma.com/file/vTrRJ0BajTkFK6d1R7rwg1/Hira-yoga?type=design&node-id=0%3A1&mode=design&t=4KlQtGxBQbrUxr49-1"
     },
     {
-        id: 20,
+        id: 21,
         image: cepheus,
         title: "cepheus",
         category: "design",
         link: "https://www.figma.com/file/nVHrMnP9HxLlkbYxREhpmM/cepheus-space?type=design&node-id=0%3A1&mode=design&t=oWDUiOoOyj4Bsl1g-1"
-    },
+    }, 
+   
 ];
 
 
